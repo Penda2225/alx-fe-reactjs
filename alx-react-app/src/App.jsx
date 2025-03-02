fffbfb6c7cf87d4c0de19034c491de175097a14b
Penda <penda25@gwu.edu>
2025-03-02 22:25:45 +0000

    user profile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,8 @@ import './App.css'
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
+
 
 
 
@@ -41,6 +43,9 @@ function App() {
             <Header />
             <MainContent />
             <Footer />
+        </div>
+        <div>
+            <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
         </div>
     </>
   )
