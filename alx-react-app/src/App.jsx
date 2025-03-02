e92734d6dc13d7b8c5078c9624392ae346218258
Penda <penda25@gwu.edu>
2025-03-02 22:12:58 +0000

    header, footer, main

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
+
 
 
 function App() {
@@ -32,6 +36,11 @@ function App() {
       </p>
       <div>
             <WelcomeMessage />
+        </div>
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
         </div>
     </>
   )
