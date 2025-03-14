90b297866c293941c66baf880afd5edce4de27b7
Penda <penda25@gwu.edu>
2025-03-14 13:27:47 +0000

    my company add

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,14 +2,19 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
     return (
-        <nav style={{ backgroundColor: 'navy', padding: '10px', textAlign: 'center' }}>
-            <Link to="/" style={{ color: 'white', margin: '10px' }}>Home</Link>
-            <Link to="/about" style={{ color: 'white', margin: '10px' }}>About</Link>
-            <Link to="/services" style={{ color: 'white', margin: '10px' }}>Services</Link>
-            <Link to="/contact" style={{ color: 'white', margin: '10px' }}>Contact</Link>
+        <nav style={{ 
+            backgroundColor: 'navy', 
+            padding: '10px', 
+            display: 'flex', 
+            justifyContent: 'center',
+            alignItems: 'center' 
+        }}>
+            <Link to="/" style={{ color: 'white', margin: '10px', textDecoration: 'none' }}>Home</Link>
+            <Link to="/about" style={{ color: 'white', margin: '10px', textDecoration: 'none' }}>About</Link>
+            <Link to="/services" style={{ color: 'white', margin: '10px', textDecoration: 'none' }}>Services</Link>
+            <Link to="/contact" style={{ color: 'white', margin: '10px', textDecoration: 'none' }}>Contact</Link>
         </nav>
     );
 }
 
 export default Navbar;
- 
