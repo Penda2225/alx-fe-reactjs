34c966ea830cad3f24ed8de2a4136d1f690eda13
Penda <penda25@gwu.edu>
2025-04-17 00:14:14 +0000

    initial setup

---
+++ tailwind-react-integration/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"], // Vite handles HTML here
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
